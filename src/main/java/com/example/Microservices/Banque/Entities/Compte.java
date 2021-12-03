59ea1f36c05b63c9c98eab06785266ec13208026
DEVGEEKTM <ismailostmar@gmail.com>
2021-12-03 02:13:36 +0100

    Adding Lombok Annotations

---
+++ src/main/java/com/example/Microservices/Banque/Entities/Compte.java
@@ -0,0 +1,25 @@
+package com.example.Microservices.Banque.Entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Date;
+
+@XmlRootElement // Annotations permet de générer un resultat en XML
+@Entity
+@Data
+@AllArgsConstructor @NoArgsConstructor @ToString
+public class Compte {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double solde;
+    @Temporal(TemporalType.DATE)
+    private Date dateCreation;
+    @Enumerated(EnumType.STRING)
+    @Column(length = 10)
+    private TypeCompte type;
+}
