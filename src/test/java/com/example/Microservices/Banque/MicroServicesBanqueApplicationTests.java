3cd929f3ad21cc41d785ccb1dc1df5bdd04026a1
DEVGEEKTM <ismailostmar@gmail.com>
2021-12-03 02:12:53 +0100

    small changes

---
+++ src/test/java/com/example/Microservices/Banque/MicroServicesBanqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Microservices.Banque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicesBanqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
