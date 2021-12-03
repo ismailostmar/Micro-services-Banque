22138a872422e528749e33cbbb46c609552b2c77
DEVGEEKTM <ismailostmar@gmail.com>
2021-12-03 02:14:42 +0100

    Try New Way with Rest Methods

---
+++ src/main/java/com/example/Microservices/Banque/Web/CompteRestControllerAPI.java
@@ -0,0 +1,43 @@
+package com.example.Microservices.Banque.Web;
+
+import com.example.Microservices.Banque.Entities.Compte;
+import com.example.Microservices.Banque.repositories.CompteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+//@RestController
+@RequestMapping("/banque")
+public class CompteRestControllerAPI {
+
+    @Autowired
+    private CompteRepository compteRepository;
+
+    @GetMapping(value = "/comptes", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public List<Compte> CompteList() {
+        return compteRepository.findAll();
+    }
+
+    @GetMapping(path = "/comptes/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Compte getOne(@PathVariable Long id) {
+        return compteRepository.findById(id).get();
+    }
+
+    @PostMapping(path = "/comptes")
+    public Compte save(Compte compte) {
+        return compteRepository.save(compte);
+    }
+
+    @PutMapping(path = "/comptes/{id}")
+    public Compte update(@RequestBody Compte compte, @PathVariable Long id) {
+        compte.setId(id);
+        return compteRepository.save(compte);
+    }
+
+    @DeleteMapping(path = "/comptes/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public void delete(@PathVariable Long id) {
+        compteRepository.deleteById(id);
+    }
+}
