0fd73ea2876927e2533f52c33f9903a86244691b
DEVGEEKTM <ismailostmar@gmail.com>
2021-12-08 02:12:48 +0100

    Adding Client Repository to save it in an Account and get his Sold.

--- src/main/java/com/example/Microservices/Banque/MicroServicesBanqueApplication.java
+++ src/main/java/com/example/Microservices/Banque/MicroServicesBanqueApplication.java
@@ -1,12 +1,15 @@
 package com.example.Microservices.Banque;
 
+import com.example.Microservices.Banque.Entities.Client;
 import com.example.Microservices.Banque.Entities.Compte;
 import com.example.Microservices.Banque.Entities.TypeCompte;
+import com.example.Microservices.Banque.repositories.ClientRepository;
 import com.example.Microservices.Banque.repositories.CompteRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 import java.util.Date;
 
@@ -18,17 +21,18 @@ public class MicroServicesBanqueApplication {
 	}
 
 	@Bean
-	CommandLineRunner start(CompteRepository compteRepository){
+	CommandLineRunner start(CompteRepository compteRepository , RepositoryRestConfiguration restConfiguration,
+							ClientRepository clientRepository){
 		return args -> {
-			compteRepository.save(new Compte(null,Math.random()*9000, new Date(), TypeCompte.EPARGNE));
-			compteRepository.save(new Compte(null,Math.random()*9000, new Date(), TypeCompte.EPARGNE));
-			compteRepository.save(new Compte(null,Math.random()*9000, new Date(), TypeCompte.COURANT));
-			compteRepository.save(new Compte(null,Math.random()*9000, new Date(), TypeCompte.EPARGNE));
-			compteRepository.save(new Compte(null,Math.random()*9000, new Date(), TypeCompte.COURANT));
-			compteRepository.save(new Compte(null,Math.random()*9000, new Date(), TypeCompte.COURANT));
-			compteRepository.save(new Compte(null,Math.random()*9000, new Date(), TypeCompte.EPARGNE));
+			restConfiguration.exposeIdsFor(Compte.class); // si on veut exposer le ID de notre Entité
+			Client c1 = clientRepository.save(new Client(null,"ISMAIL",null));
+			Client c2 = clientRepository.save(new Client(null,"ALI",null));
+			compteRepository.save(new Compte(null,Math.random()*9000, new Date(), TypeCompte.EPARGNE , c1));
+			compteRepository.save(new Compte(null,Math.random()*9000, new Date(), TypeCompte.EPARGNE ,c2));
+			compteRepository.save(new Compte(null,Math.random()*9000, new Date(), TypeCompte.COURANT,c2));
+			compteRepository.save(new Compte(null,Math.random()*9000, new Date(), TypeCompte.EPARGNE,c1));
 			compteRepository.findAll().forEach(c -> {
-				System.out.println(c.toString());
+				System.out.println(c.getSolde()); // Lazy
 			});
 		};
 	}
