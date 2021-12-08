8a1557c88a8d15fb500df615ea425a5364deba90
DEVGEEKTM <ismailostmar@gmail.com>
2021-12-08 03:09:47 +0100

    Deploy a Web Service SOAP with Simple Jax Ws Service Exporter

--- src/main/java/com/example/Microservices/Banque/MyConfig.java
+++ src/main/java/com/example/Microservices/Banque/MyConfig.java
@@ -5,6 +5,7 @@ import com.example.Microservices.Banque.Web.CompteRestJaxRSAPI;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.remoting.jaxws.SimpleJaxWsServiceExporter;
 
 // Class de configuration permettant de faire le deploiement avec Spring JAX-RS
 @Configuration
@@ -22,6 +23,13 @@ public class MyConfig {
         ResourceConfig jerseyServlet = new ResourceConfig();
         jerseyServlet.register(CompteRestJaxRSAPI.class);
         return jerseyServlet;
+    }
+
+    @Bean // Deployer un Web Service JAX-WS en utilisant SimpleJaxServiceExporter
+    SimpleJaxWsServiceExporter serviceExporter(){
+        SimpleJaxWsServiceExporter serviceExporter = new SimpleJaxWsServiceExporter();
+        serviceExporter.setBaseAddress("http://0.0.0.0:8888/");
+        return serviceExporter;
     }
 }
 
